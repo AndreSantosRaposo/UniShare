7a295e5e97425f7394cf12a4aa5feddf26a318cc
AndreSantosRaposo <andresantosraposo@gmail.com>
2024-07-24 21:17:24 +0100

    Criação da NavBar, e setup de variaveis de bootstrap

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import "./components/custom.scss"
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
+import '../../dist/css/custom.css'
 
 function App() {
 
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,28 +1,36 @@
+
+
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-primary">
-          <div className="container-fluid px-5 d-flex align-items-center justify-content-between">
-              <img src="/public/imgs/UNISHARElogo.png" alt="Logo" height="34" className="d-inline-block align-text-top me-2"/> {/* Add margin to the right of the image */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-              <ul className="navbar-nav mb-2 mb-lg-0 fs-5">
-                <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Disciplinas</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Upload</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Perfil</a></li>
-              </ul>
+            <div className="container w-90">
+                <a className="navbar-brand d-flex align-items-center" href="/">
+                    <img src="/public/imgs/UNISHARElogo.png" alt="Logo" height="34" />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-lg-0 fs-5">
+                        <li className="nav-item"><a className="nav-link" href="#">Disciplinas</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Upload</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Perfil</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Contactos</a></li>
+                        <li className="nav-item d-lg-none"><a className="nav-link" href="#">Log In</a></li>
+                    </ul>
+                </div>
+                <div className="d-none d-lg-flex align-items-center">
+                    <button className='btn btn-secondary text-tertiary'>Log In</button>
+                </div>
             </div>
-          </div>
         </nav>
-      );
+    );
 }
